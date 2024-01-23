504778df1411dcfcd40ee95510ef672a1dd3dfa1
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2024-01-23 20:04:49 -0300

    Auth Schema created, we have a new security method to user data joining

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 import Router from 'express-promise-router';
 import { AuthController } from '../controllers/auth.js';
 import { isAuth } from '../middlewares/auth.middleware.js';
+import { signupSchema, signinSchema } from '../schemas/auth.schema.js';
+import { validateSchema } from '../middlewares/validate.middleware.js';
 
 export const authRouter = Router()
 
-authRouter.post('/signin', AuthController.signin)
-authRouter.post('/signup', AuthController.singup)
+authRouter.post('/signin', validateSchema(signinSchema), AuthController.signin)
+authRouter.post('/signup', validateSchema(signupSchema), AuthController.singup)
 authRouter.post('/logout', AuthController.logout)
 authRouter.get('/profile', isAuth, AuthController.profile)
---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+export const signupSchema = z.object({
+  name: z
+    .string({
+      required_error: 'El nombre es requerido',
+      invalid_type_error: 'El nombre debe ser un texto'
+    })
+    .min(1)
+    .max(50),
+  email: z
+    .string({
+      required_error: 'El email es requerido',
+      invalid_type_error: 'El email debe ser un texto'
+    })
+    .email({message: 'Ingrese un email valido'}),
+  password: z
+    .string({
+      required_error: 'La contraseña es requerida',
+      invalid_type_error: 'La contraseña debe ser un texto'
+    })
+    .min(6, {
+      message: 'La contraseña debe tener al menos 6 caracteres'
+    })
+    .max(100)
+})
+
+export const signinSchema = z.object({
+  email: z.string({
+    required_error: 'El email es requerido',
+    invalid_type_error: 'El email debe ser un texto'
+  }).email({message: 'Ingrese un email valido'}),
+  password: z.string({
+    required_error: 'La contraseña es requerida',
+    invalid_type_error: 'La contraseña debe ser un texto'
+  }).min(6, {
+    message: 'La contraseña debe tener al menos 6 caracteres'
+  })
+})
